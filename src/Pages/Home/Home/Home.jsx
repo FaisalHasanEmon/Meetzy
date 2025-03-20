bbbfeac2e5af125cc9fa6596fb6614b3a30e7a75
Rayhan-50 <rayhanahmed.nstu@gmail.com>
2025-03-20 10:30:52 +0600

    remove unnessary code

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,31 +1,3 @@
-// import { Link } from "react-router-dom";
-
-// function Home() {
-//   return (
-//     <div className="flex flex-col items-center justify-center h-[80vh] text-center">
-//       <h1 className="text-4xl font-bold">Welcome to Meetzy</h1>
-//       <p className="mt-4 text-gray-600">
-//         High-quality video calls anytime, anywhere.
-//       </p>
-//       <div className="flex justify-center items-center gap-5">
-//         <Link
-//           to="/call"
-//           className="mt-6 px-6 py-2 bg-blue-600 text-white font-bold rounded-lg"
-//         >
-//           Create a new meeting
-//         </Link>
-//         <Link
-//           to="/call"
-//           className="mt-6 px-6 py-2 bg-blue-600 text-white font-bold rounded-lg"
-//         >
-//           Join a meeting
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
 
 
 import React, { useState, useRef, useContext } from 'react';
