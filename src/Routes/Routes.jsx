28d0e934123afe0d86be06c4a27af9ec13fb946c
unknown <alazim-star>
2025-04-11 19:06:28 +0600

    add final for presentation

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -11,6 +11,7 @@ import AboutUs from "../Pages/AboutUs/AboutUs";
 import FeaturePage from "../Pages/FeaturePage/FeaturePage";
 import VideoCall from "../Pages/VideoCall/VideoCall";
 import WhyMeetzy from "../Pages/WhyMeetzy/WhyMeetzy";
+import Profile from "../Pages/Profile";
 
 export const router = createBrowserRouter([
   {
@@ -26,17 +27,14 @@ export const router = createBrowserRouter([
         ),
       },
       {
-        path: "/call",
+        path: "/call/:meetingCode",
         element: (
           <PrivateRoute>
             <CallInterface />
           </PrivateRoute>
         ),
       },
-      {
-        path: "/call/:meetingCode",
-        element: <VideoCall></VideoCall>,
-      },
+    
       {
         path: "/features",
         element: (
@@ -57,6 +55,10 @@ export const router = createBrowserRouter([
       {
         path: "aboutUs",
         element: <AboutUs></AboutUs>,
+      },
+      {
+        path: "profile",
+        element: <Profile></Profile>,
       },
       /*added private routes for the welcome page*/
       {
